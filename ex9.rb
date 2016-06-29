f4363ce6a558bf1de5e7bbc1d75aacd34caf9cad
marcoderoh <marcoderoh@gmail.com>
2016-06-29 21:56:59 +0300

    exercise 10

--- ex9.rb
+++ ex9.rb
@@ -1,13 +1,14 @@
-formatter = "%{first} %{second} %{third} %{fourth}"
+# Here's some new strange stuff, remember type it exactly.
 
-puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
-puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
-puts formatter % {first: true, second: false, third: true, fourth: false}
-puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
-puts formatter % {
-  first: "I had this thing.",
-  second: "That you could type up right.",
-  third: "But it didn't sing.",
-  fourth: "So I said goodnight."
-}
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
